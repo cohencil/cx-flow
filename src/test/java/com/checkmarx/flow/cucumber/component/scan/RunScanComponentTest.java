89ca876dc69f9dbc7b14bb81d4c8c73ba0615aeb
tala <tala@TALA-PC>
2020-03-05 11:44:02 +0200

    componentTest added

--- src/test/java/com/checkmarx/flow/cucumber/component/scan/RunScanComponentTest.java
+++ src/test/java/com/checkmarx/flow/cucumber/component/scan/RunScanComponentTest.java
@@ -1,16 +1,15 @@
 package com.checkmarx.flow.cucumber.component.scan;
 
-import com.checkmarx.test.flow.config.CxFlowMocksConfig;
-import com.checkmarx.flow.CxFlowApplication;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
-import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles()
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin = { "pretty", "summary", "html:build/cucumber/component/scan", "json:build/cucumber/component/scan/cucumber.json" },
+@CucumberOptions(plugin = {"pretty", "summary", "html:build/cucumber/component/scan", "json:build/cucumber/component/scan/cucumber.json"},
         features = "src/test/resources/cucumber/features",
-        glue = { "com.checkmarx.flow.cucumber.common.steps", "com.checkmarx.flow.cucumber.component.scan" },
+        glue = {"com.checkmarx.flow.cucumber.common.steps", "com.checkmarx.flow.cucumber.component.scan"},
         tags = " @ScanFeature and @ComponentTest and not @Skip")
 public class RunScanComponentTest {
 }
