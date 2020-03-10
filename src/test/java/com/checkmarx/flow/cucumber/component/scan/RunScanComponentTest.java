488dc2aee430277b21cec5d6638c175912c12b99
Tala <tal.akalo@checkmarx.com>
2020-03-10 17:18:16 +0200

    ActiveProfiles

--- src/test/java/com/checkmarx/flow/cucumber/component/scan/RunScanComponentTest.java
+++ src/test/java/com/checkmarx/flow/cucumber/component/scan/RunScanComponentTest.java
@@ -1,12 +1,11 @@
 package com.checkmarx.flow.cucumber.component.scan;
 
-import com.checkmarx.test.flow.config.CxFlowMocksConfig;
-import com.checkmarx.flow.CxFlowApplication;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
-import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles()
 @RunWith(Cucumber.class)
 @CucumberOptions(plugin = {"pretty", "summary", "html:build/cucumber/component/scan", "json:build/cucumber/component/scan/cucumber.json"},
         features = "src/test/resources/cucumber/features",
