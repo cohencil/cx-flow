ca5cfbd4896eb47f57bf35238b1ada890ae7fe6c
Tala <tal.akalo@checkmarx.com>
2020-03-11 10:28:59 +0200

    GitHubToJiraSteps added to glue

--- src/test/java/com/checkmarx/flow/cucumber/integration/end2end/github2jira/RunEndToEndGitHubToJiraTest.java
+++ src/test/java/com/checkmarx/flow/cucumber/integration/end2end/github2jira/RunEndToEndGitHubToJiraTest.java
@@ -1,14 +1,13 @@
 package com.checkmarx.flow.cucumber.integration.end2end.github2jira;
 
-import org.junit.runner.RunWith;
-
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin = { "pretty", "summary", "html:build/integration/end2end/github2jira", "json:build/cucumber/features/e2eTests/githubToJira" },
+@CucumberOptions(plugin = {"pretty", "summary", "html:build/integration/end2end/github2jira", "json:build/cucumber/features/e2eTests/githubToJira"},
         features = "src/test/resources/cucumber/features",
-        glue = { "com.checkmarx.flow.cucumber.integration.end2end.github2jira"},
+        glue = {"com/checkmarx/flow/cucumber/integration/end2end/github2jira/GitHubToJiraSteps", "com.checkmarx.flow.cucumber.integration.end2end.github2jira"},
         tags = "@EndToEnd and @Integration and not @Skip")
 public class RunEndToEndGitHubToJiraTest {
 }
